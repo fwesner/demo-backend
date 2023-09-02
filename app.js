3c7ab535de97ca89f02f340942a53ee8dc2418ed
Francisco Wesner <franciscowesner@hotmail.com>
2023-09-02 20:01:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
 const userModel = require('./models/User');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
