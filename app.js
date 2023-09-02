783eac9ffe61691d3732247f3076fd4a8a6facdb
Francisco Wesner <franciscowesner@hotmail.com>
2023-09-02 19:32:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const { Sequelize } = require('sequelize');
+const userModel = require('./models/User');
 
 const app = express();
 const port = 3000;
 
-// Conectar a la base de datos SQLite (o crea una si no existe)
-const db = new sqlite3.Database('mydb.db');
-
-// Crear una tabla y agregar algunos datos de ejemplo
-db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS users (id INT, name TEXT)");
-  const stmt = db.prepare("INSERT INTO users VALUES (?, ?)");
-  stmt.run(1, "Usuario 1");
-  stmt.run(2, "Usuario 2");
-  stmt.finalize();
+// Configuración de Sequelize y conexión a la base de datos SQLite
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'mydb.sqlite', // Nombre de la base de datos SQLite
 });
 
-// Endpoint para obtener todos los usuarios
-app.get('/api/users', (req, res) => {
-  db.all("SELECT * FROM users", (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json(rows);
+// Definir el modelo User
+const User = userModel(sequelize);
+
+// Sincronizar el modelo con la base de datos (crear la tabla si no existe)
+sequelize.sync()
+  .then(() => {
+    console.log('Base de datos sincronizada.');
+  })
+  .catch((err) => {
+    console.error('Error al sincronizar la base de datos:', err);
   });
+
+// Endpoint para obtener todos los usuarios
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener usuarios.' });
+  }
 });
 
 app.listen(port, () => {
